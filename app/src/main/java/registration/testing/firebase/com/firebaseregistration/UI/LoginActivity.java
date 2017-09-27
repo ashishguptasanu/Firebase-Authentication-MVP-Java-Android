340af3e87989182ee08c77f010e8b3a12e54c199
Ashish <ashish.ashish.gupta745@gmail.com>
2017-09-27 14:45:14 +0530

    Completed

--- app/src/main/java/registration/testing/firebase/com/firebaseregistration/UI/LoginActivity.java
+++ app/src/main/java/registration/testing/firebase/com/firebaseregistration/UI/LoginActivity.java
@@ -1,15 +1,91 @@
 package registration.testing.firebase.com.firebaseregistration.UI;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import registration.testing.firebase.com.firebaseregistration.Core.Login.LoginContract;
+import registration.testing.firebase.com.firebaseregistration.Core.Login.LoginPresenter;
 import registration.testing.firebase.com.firebaseregistration.R;
 
-public class LoginActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener, LoginContract.View{
+    Button btnLogin;
+    TextView tvRegister;
+    EditText edtEmail, edtPassword;
+    private LoginPresenter mLoginPresenter;
+    ProgressDialog mProgressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initViews();
+
+    }
+
+    private void initViews() {
+        btnLogin = (Button)findViewById(R.id.button_login);
+        btnLogin.setOnClickListener(this);
+        tvRegister = (TextView)findViewById(R.id.tv_register);
+        tvRegister.setOnClickListener(this);
+        edtEmail = (EditText)findViewById(R.id.email_login);
+        edtPassword = (EditText)findViewById(R.id.password_login);
+
+        mLoginPresenter = new LoginPresenter(this);
+
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setMessage("Please wait, Logging in..");
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button_login:
+                checkLoginDetails();
+                break;
+            case R.id.tv_register:
+                moveToRegisterActivity();
+                break;
+        }
+    }
+
+    private void moveToRegisterActivity() {
+        Intent intent = new Intent(getApplicationContext(), RegistrationActivity.class);
+        startActivity(intent);
+    }
+
+    private void checkLoginDetails() {
+        if(!TextUtils.isEmpty(edtEmail.getText().toString()) && !TextUtils.isEmpty(edtPassword.getText().toString())){
+            initLogin(edtEmail.getText().toString(), edtPassword.getText().toString());
+        }else{
+            if(TextUtils.isEmpty(edtEmail.getText().toString())){
+                edtEmail.setError("Please enter a valid email");
+            }if(TextUtils.isEmpty(edtPassword.getText().toString())){
+                edtPassword.setError("Please enter password");
+            }
+        }
+    }
+
+    private void initLogin(String email, String password) {
+        mProgressDialog.show();
+        mLoginPresenter.login(this, email, password);
+    }
+
+    @Override
+    public void onLoginSuccess(String message) {
+        mProgressDialog.dismiss();
+        Toast.makeText(getApplicationContext(), "Successfully Logged in" , Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLoginFailure(String message) {
+        mProgressDialog.dismiss();
+        Toast.makeText(getApplicationContext(),message , Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/registration/testing/firebase/com/firebaseregistration/UI/RegistrationActivity.java
+++ app/src/main/java/registration/testing/firebase/com/firebaseregistration/UI/RegistrationActivity.java
@@ -1,15 +1,93 @@
 package registration.testing.firebase.com.firebaseregistration.UI;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseUser;
+
+import registration.testing.firebase.com.firebaseregistration.Core.Registration.RegistrationContract;
+import registration.testing.firebase.com.firebaseregistration.Core.Registration.RegistrationPresenter;
 import registration.testing.firebase.com.firebaseregistration.R;
 
-public class RegistrationActivity extends AppCompatActivity {
-
+public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener, RegistrationContract.View {
+    TextView tvLogin;
+    Button btnRegistration;
+    EditText edtEmail, edtPassword;
+    private RegistrationPresenter mRegisterPresenter;
+    ProgressDialog mPrgressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        initViews();
+    }
+    private void initViews() {
+        btnRegistration = (Button)findViewById(R.id.btn_register);
+        btnRegistration.setOnClickListener(this);
+        tvLogin = (TextView)findViewById(R.id.tv_login);
+        tvLogin.setOnClickListener(this);
+        edtEmail = (EditText)findViewById(R.id.email_register);
+        edtPassword = (EditText)findViewById(R.id.password_register);
+
+        mRegisterPresenter = new RegistrationPresenter(this);
+
+        mPrgressDialog = new ProgressDialog(this);
+        mPrgressDialog.setMessage("Please wait, Adding profile to database.");
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_register:
+                checkRegistrationDetails();
+                break;
+            case R.id.tv_login:
+                moveToLoginActivity();
+                break;
+        }
+    }
+
+    private void moveToLoginActivity() {
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
+    }
+
+    private void checkRegistrationDetails() {
+        if(!TextUtils.isEmpty(edtEmail.getText().toString()) && !TextUtils.isEmpty(edtPassword.getText().toString())){
+            initLogin(edtEmail.getText().toString(), edtPassword.getText().toString());
+        }else{
+            if(TextUtils.isEmpty(edtEmail.getText().toString())){
+                edtEmail.setError("Please enter a valid email");
+            }if(TextUtils.isEmpty(edtPassword.getText().toString())){
+                edtPassword.setError("Please enter password");
+            }
+        }
+    }
+
+    private void initLogin(String email, String password) {
+        mPrgressDialog.show();
+        mRegisterPresenter.register(this, email, password);
+    }
+
+    @Override
+    public void onRegistrationSuccess(FirebaseUser firebaseUser) {
+        mPrgressDialog.dismiss();
+        Toast.makeText(getApplicationContext(), "Successfully Registered" , Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRegistrationFailure(String message) {
+        mPrgressDialog.dismiss();
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
